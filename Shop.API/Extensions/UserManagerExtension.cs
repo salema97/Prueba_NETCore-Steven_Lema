8a20f1fa884ae3b82e9e78ca758f6af97c3e1b68
salema97 <salemavelasquez97@gmail.com>
2024-05-06 20:21:16 -0500

    Fix user registration error

--- Shop.API/Extensions/UserManagerExtension.cs
+++ Shop.API/Extensions/UserManagerExtension.cs
@@ -10,14 +10,14 @@ namespace Shop.API.Extensions
         public static async Task<AppUser?> FindUserByClaimPrincipamWithAddress(this UserManager<AppUser> userManager, ClaimsPrincipal user)
         {
             var email = user?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
-            return await userManager.Users.Include(x => x.Address).SingleOrDefaultAsync();
+            return await userManager.Users.Include(x => x.Address).SingleOrDefaultAsync(x => x.Email == email);
 
         }
 
         public static async Task<AppUser?> FindEmailByClaimPrincipal(this UserManager<AppUser> userManager, ClaimsPrincipal user)
         {
             var email = user?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
-            return await userManager.Users.SingleOrDefaultAsync();
+            return await userManager.Users.SingleOrDefaultAsync(x => x.Email == email);
         }
     }
 }
